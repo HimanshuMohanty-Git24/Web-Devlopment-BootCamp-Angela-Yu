07347dab0e243e62558cddefa65edc66565ef5ba
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 22:27:59 +0530

    FizzBuzz
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ JS/FizzBuzz.js
@@ -0,0 +1,24 @@
+var arr = [];
+count = 1;
+
+function FizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+    arr.push("FizzBuzz");
+  }
+  else if (count % 3 === 0) {
+    arr.push("Fizz");
+  }
+  else if (count % 5 === 0) {
+    arr.push("Buzz");
+  }
+  else {
+    arr.push(count);
+  }
+  count++;
+  console.log(arr);
+}
+FizzBuzz();
+FizzBuzz();
+FizzBuzz();
+FizzBuzz();
+FizzBuzz();
