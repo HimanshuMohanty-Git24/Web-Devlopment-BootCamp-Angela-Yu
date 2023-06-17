803b69dbebf261cf9b62381dbab4563d896fcb30
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 21:04:14 +0530

    Love Calc
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- JS/LoveCalc.js
+++ JS/LoveCalc.js
@@ -3,4 +3,9 @@ var name2=prompt("Enter your partner number");
 var num =Math.random();
 num = (num*100)+1;
 num = Math.floor(num);
-alert("These are the results of the calculations by Dr. Love:"+num+"%");
+if(num===100){
+    alert("These are the results of the calculations by Dr. Love:"+num+"%"+" you love each other like Ram Loves Sita");
+}
+else{
+    alert("These are the results of the calculations by Dr. Love:"+num+"%");
+}
