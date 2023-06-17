a73c7a3c842f793332ac2a466ccfbbc48db2c804
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 23:02:25 +0530

    Beer Bottle
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ JS/BottlesOfBeer.js
@@ -0,0 +1,6 @@
+var NoOFBeer = 99;
+for (let i = 99; i >1;i--) {
+    console.log(i+" bottles of beer on the wall, "+i+" bottles of beer.Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.\n");
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.\n")
+console.log("No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.\n");
