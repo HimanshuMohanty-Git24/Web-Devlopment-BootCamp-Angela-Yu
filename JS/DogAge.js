c5e5d83c02b3c1f3fd6521a5e0289a43d4a7cbf0
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 18:52:44 +0530

    Dog Age calc
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- JS/DogAge.js
+++ JS/DogAge.js
@@ -1,3 +1,4 @@
 var dogage = prompt("How old is your dog?");
 var humanage = ((dogage - 2) * 4) + 21;
 alert("Your dog is " + humanage + " years old in human years.");
+console.log("Your dog is " + humanage + " years old in human years.");
