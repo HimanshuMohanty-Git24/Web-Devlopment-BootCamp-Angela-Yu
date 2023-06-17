b7a89aca47f04dc0b89694dc48922ef74c614fc4
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 12:20:02 +0530

    Js pratice

---
+++ JS/concat.js
@@ -0,0 +1,4 @@
+var message = "Hello";
+var name = "Rahul";
+var greeting = message + " " + name;
+console.log(greeting);
---
+++ JS/swap.js
@@ -0,0 +1,8 @@
+var a = 3;
+var b=8;
+
+var temp=a;
+a=b;
+b=temp;
+console.log("a is "+a);
+console.log("b is "+b);
