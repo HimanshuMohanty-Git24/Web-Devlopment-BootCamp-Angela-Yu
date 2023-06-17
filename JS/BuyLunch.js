c7a623b4c2fadf43689032a02101b63562da7ef3
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-17 22:46:33 +0530

    Random Lunch Byer
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ JS/BuyLunch.js
@@ -0,0 +1,4 @@
+var Pep = ["Ram","Shyam","Rahul","Modi","Shilu"];
+var RandPepNo=Math.random()*Pep.length;
+RandPepNo=Math.floor(RandPepNo);
+console.log("Today's Bill Will be paid by:"+Pep[RandPepNo]);
