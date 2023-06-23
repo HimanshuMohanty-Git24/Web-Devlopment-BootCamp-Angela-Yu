44613ffd165d1b3c412e099b3b1ecf78e7c36a8e
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-23 22:29:20 +0530

    Intro to node
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+const superheroes = require('superheroes');
+
+var mySuperheroName=superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(mySuperheroName);
+
+
+const  supervillains = require('superheroes');
+var mySuperVillains = supervillains.random();
+console.log(mySuperVillains);
