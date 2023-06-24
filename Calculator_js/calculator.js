4094620bff9796b714bb9ce8da8eb23b783c0a96
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-06-24 12:04:40 +0530

    Bmi Calculator
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- Calculator_js/calculator.js
+++ Calculator_js/calculator.js
@@ -14,3 +14,15 @@ app.post("/",function(req,res){
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
+
+// BMI calculator
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmicalculator.html");
+});
+app.post("/bmicalculator",function(req,res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var Bmi = weight/(Math.pow(height,2));
+    res.send("Your BMI is "+Bmi);
+});
