7552b7d5da98dd21d8aaaa44d3c5c4de846e5e91
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-07-06 10:15:40 +0530

    Nalco-Mis

--- Nalco-Mis/script.js
+++ Nalco-Mis/script.js
@@ -4,56 +4,56 @@ var formattedDate = currentDate.toISOString().substring(0, 10);
 document.getElementById("current-date").value = formattedDate;
 document.getElementById("rundate").value = formattedDate;
 
-
-//Calculate the diffrence gen mwhr
-var startInput = document.getElementsByName("ga_mwhr_s")[0];
-var endInput = document.getElementsByName("ga_mwhr_e")[0];
+// Calculate the difference gen mwhr
+var startInputGenMwhr = document.getElementsByName("ga_mwhr_s")[0];
+var endInputGenMwhr = document.getElementsByName("ga_mwhr_e")[0];
 var genActInput = document.getElementsByName("gen_act")[0];
-startInput.addEventListener("input", calculateDifference);
-endInput.addEventListener("input", calculateDifference);
-function calculateDifference() {
-  var startValue = parseFloat(startInput.value);
-  var endValue = parseFloat(endInput.value);
+
+startInputGenMwhr.addEventListener("input", calculateDifferenceGenMwhr);
+endInputGenMwhr.addEventListener("input", calculateDifferenceGenMwhr);
+
+function calculateDifferenceGenMwhr() {
+  var startValue = parseFloat(startInputGenMwhr.value);
+  var endValue = parseFloat(endInputGenMwhr.value);
+  
   if (!isNaN(startValue) && !isNaN(endValue)) {
-    var difference = endValue - startValue;
-    difference = Math.abs(difference);
+    var difference = Math.abs(endValue - startValue);
     genActInput.value = difference;
   }
 }
 
-
-//Calculate the diffrence gen mvar
-var startInput = document.getElementsByName("ga_mvar_s")[0];
-var endInput = document.getElementsByName("ga_mvar_e")[0];
+// Calculate the difference gen mvar
+var startInputGenMvar = document.getElementsByName("ga_mvar_s")[0];
+var endInputGenMvar = document.getElementsByName("ga_mvar_e")[0];
 var genReactInput = document.getElementsByName("gen_react")[0];
-startInput.addEventListener("input", calculateAbsoluteDifference);
-endInput.addEventListener("input", calculateAbsoluteDifference);
-function calculateAbsoluteDifference() {
-  var startValue = parseFloat(startInput.value);
-  var endValue = parseFloat(endInput.value);
+
+startInputGenMvar.addEventListener("input", calculateAbsoluteDifferenceGenMvar);
+endInputGenMvar.addEventListener("input", calculateAbsoluteDifferenceGenMvar);
+
+function calculateAbsoluteDifferenceGenMvar() {
+  var startValue = parseFloat(startInputGenMvar.value);
+  var endValue = parseFloat(endInputGenMvar.value);
+
   if (!isNaN(startValue) && !isNaN(endValue)) {
     var absoluteDifference = Math.abs(endValue - startValue);
     genReactInput.value = absoluteDifference;
   }
 }
 
-
-//Calculate the diffrence uat mwhr
-var startInput = document.getElementsByName("ua_mwhr_s")[0];
-var endInput = document.getElementsByName("ua_mwhr_e")[0];
+// Calculate the difference uat mwhr
+var startInputUatMwhr = document.getElementsByName("ua_mwhr_s")[0];
+var endInputUatMwhr = document.getElementsByName("ua_mwhr_e")[0];
 var uatActInput = document.getElementsByName("uat_act")[0];
 
-startInput.addEventListener("input", calculateAbsoluteDifference);
-endInput.addEventListener("input", calculateAbsoluteDifference);
+startInputUatMwhr.addEventListener("input", calculateAbsoluteDifferenceUatMwhr);
+endInputUatMwhr.addEventListener("input", calculateAbsoluteDifferenceUatMwhr);
 
-function calculateAbsoluteDifference() {
-  var startValue = parseFloat(startInput.value);
-  var endValue = parseFloat(endInput.value);
+function calculateAbsoluteDifferenceUatMwhr() {
+  var startValue = parseFloat(startInputUatMwhr.value);
+  var endValue = parseFloat(endInputUatMwhr.value);
 
   if (!isNaN(startValue) && !isNaN(endValue)) {
     var absoluteDifference = Math.abs(endValue - startValue);
     uatActInput.value = absoluteDifference;
   }
 }
-
-
