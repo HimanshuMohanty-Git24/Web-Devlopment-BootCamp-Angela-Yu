400bed2873bfad93f6decb91f16dd6ef6d41eaa8
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-07-18 22:39:18 +0530

    Changes
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,17 +2,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
     var today = new Date();
-    var currentDay = today.getDate();
-     
-    if(currentDay===6||currentDay===0){
-        res.send("<h1>Yay it's the weekend!</h1>");
+    var currentDay = today.getDay();
+    var day = "";
+    var msg="";
+
+    switch(currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: "+currentDay);
+    }
+    if(currentDay===0 || currentDay===6){
+        msg= "Happy Weekend!";
     }
     else{
-        res.sendFile(__dirname+"/index.html");
+        msg= "Work Hard Buddy!";
     }
+    res.render("list",({
+        KindOfDay: day
+        ,Message: msg}));
 })
 
 app.listen(3000,function () {
