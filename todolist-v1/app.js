5cd5a59a69b0046a9df7d8e64381bbd8622d2115
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-07-19 01:00:54 +0530

    TodoList
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,50 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+var items =["Do Devlopment","Work on Project","Start freelancing"];
 app.get("/",function(req,res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    var msg="";
-
-    switch(currentDay){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: "+currentDay);
+    var options= {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
     }
-    if(currentDay===0 || currentDay===6){
-        msg= "Happy Weekend!";
-    }
-    else{
-        msg= "Work Hard Buddy!";
-    }
+    var day =today.toLocaleDateString("en-US",options);
     res.render("list",({
-        KindOfDay: day
-        ,Message: msg}));
+        KindOfDay: day,
+        newListItems:items
+    }));
 })
 
+app.post("/", function(req,res){
+    var item =req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 app.listen(3000,function () {
     console.log("server started on port 3000");
 })
