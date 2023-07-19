bb0dd81e56a440266fe91c5d9b664ecd91a6b3d1
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-07-19 22:18:51 +0530

    App.js
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-let items =["Do Devlopment","Work on Project","Start freelancing"];
+
+let items =[];
+let workItems = [];
+
 app.get("/",function(req,res){
-    let today = new Date();
-    let options= {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-    let day =today.toLocaleDateString("en-US",options);
+
+    let day = date.getDate();
     res.render("list",({
         listTitle: day,
         newListItems:items
     }));
 })
 
+
+app.get("/work",function(req,res){
+    res.render("list",({listTitle:"Work List",newListItems:workItems}));
+});
+
 app.post("/", function(req,res){
     let item =req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    console.log(req.body);
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
 })
+
 app.listen(3000,function () {
     console.log("server started on port 3000");
 })
